dd416c3a18c45aaf827f6435ea2adc0bb0b7af99
FATEsys <87305846+FATEsys@users.noreply.github.com>
2022-11-10 14:08:08 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("November 11, 2022 08:00:00").getTime();
+var target_date = new Date("November 11, 2022 21:00:00").getTime();
 //var target_date = new Date(2022, 11, 11, 8, 0, 0).getTime();
  
 // variables for time units
